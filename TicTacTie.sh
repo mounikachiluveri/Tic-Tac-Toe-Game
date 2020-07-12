83f9910e11c7e7307912f29a04074bf32b46de57
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-12 09:42:01 +0530

    [Mounika] Refactor:the code changed indentation

--- TicTacTie.sh
+++ TicTacTie.sh
@@ -5,86 +5,83 @@ row=3
 column=3
 numberOfShell=$(($row*$column))
 
-function resetBoard(){
-        board=( 1 2 3 4 5 6 7 8 9 )
-        for (( i=0,j=0;j<$row;i=$(($i+3)),j++ ))
-        do
-        echo -e "\t${board[i]} | ${board[i+1]} | ${board[i+2]}"
-        if (($j!=$(($row-1))))
-        then
-        echo -e "\t---------"
-        fi
+    function resetBoard(){
+                      board=( 1 2 3 4 5 6 7 8 9 )
+                      for (( i=0,j=0;j<$row;i=$(($i+3)),j++ ))
+                      do
+                               echo -e "\t${board[i]} | ${board[i+1]} | ${board[i+2]}"
+                                   if (($j!=$(($row-1))))
+                                   then
+                                         echo -e "\t---------"
+                                   fi
 
+                     done
 
-        done
+                       }
+         function toss(){
+                        checkToss=$((RANDOM%2))
+                        if (($checkToss == 1 ))
+                        then
+                            echo -e "\nPlayer 1 plays First\n"
+                            playerChance=1
+                        else
+                            echo -e "\nComputer plays First\n"
+                            playerChance=0
+                        fi
+                       }
 
-}
-function toss(){
+              function assignSymbol(){
+                                   assign=$((RANDOM%2))
+                                   if (($assign==1))
+                                   then
+                                           playerSymbol='X'
+                                           computerSymbol='O'
+                                   else
+                                           playerSymbol='O'
+                                           computerSymbol='X'
+                                   fi
 
-        checkToss=$((RANDOM%2))
+                                  echo -e "\nPlayer 1 = $playerSymbol"
+                                  echo -e "Computer = $computerSymbol"
 
-        if (($checkToss == 1 ))
-        then
-        echo -e "\nPlayer 1 plays First\n"
-        playerChance=1
-        else
-        echo -e "\nComputer plays First\n"
-        playerChance=0
-        fi
-}
-function assignSymbol(){
-        assign=$((RANDOM%2))
-
-        if (($assign==1))
-        then
-        playerSymbol='X'
-        computerSymbol='O'
-        else
-        playerSymbol='O'
-        computerSymbol='X'
-        fi
-
-        echo -e "\nPlayer 1 = $playerSymbol"
-        echo -e "Computer = $computerSymbol"
-
-}
+                                   }
 assignSymbol
 toss
 resetBoard
 
-function updatedBoard() {
+        function updatedBoard() {
 
-        for (( i=0,j=0;j<$row;i=$(($i+3)),j++ ))
- do
- echo -e "\t${board[i]} | ${board[i+1]} | ${board[i+2]}"
- if (($j!=$(($row-1))))
- then
- echo -e "\t---------"
- fi
- done
+                               for (( i=0,j=0;j<$row;i=$(($i+3)),j++ ))
+                               do
+                                           echo -e "\t${board[i]} | ${board[i+1]} | ${board[i+2]}"
+                                                if (($j!=$(($row-1))))
+                                                then
+                                                       echo -e "\t---------"
+                                                 fi
+                               done
+                               }
 
+         function playerInput() {
 
-}
-function playerInput() {
+                         echo -e "\nPlayers 1's Turn:"
+                         while ((1))
+                         do
+                              read -p "Select shell number: " shellNumber
+                              if (( ($shellNumber<1) || ($shellNumber>9)))
+                              then
+                                     echo "Invalid, input 1...9"
+                              elif (($shellNumber!=${board[$shellNumber-1]}))
+                              then
+                                     echo -e "Invalid: Shell is already occupied, choose again"
 
-        echo -e "\nPlayers 1's Turn:"
-        while ((1))
-        do
-        read -p "Select shell number: " shellNumber
-        if (( ($shellNumber<1) || ($shellNumber>9)))
-        then
-        echo "Invalid, input 1...9"
-        elif (($shellNumber!=${board[$shellNumber-1]}))
-        then
-        echo -e "Invalid: Shell is already occupied, choose again"
-
-        else
-        break
-        fi
-        done
-        board[$shellNumber-1]=$playerSymbol
-        updatedBoard
-}
+                              else
+                              break
+                              fi
+                        
+done
+                               board[$shellNumber-1]=$playerSymbol
+                               updatedBoard
+                    }
 playerInput
 
 
